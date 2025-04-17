68b1cd60392607d53791a885364da166d5bbb3ff
10AN4 <ioanapalade09@icloud.com>
2025-04-18 00:59:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openArticles (){
+    window.location.href ='pagina.html';
+}
+function openSuggestions(){
+    window.location.href ='recap.html';
+
+}
+function openContact(){
+    Window.location.href = 'reviews.html';
+}
